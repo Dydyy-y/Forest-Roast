97c495239956f04853eefff6335b1ac206d43703
Dydy <210338140+Dydyy-y@users.noreply.github.com>
2026-03-03 00:33:22 +0100

    docs: stricmode

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,7 @@ import theme from './theme';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  /** React.StrictMode : est un wrapper qui active des vérifications supplémentaires en mode développement (désactivé en production)
-   * vérif : - détecte les effets de bord inattendus
-   * - avertit sur les APIs dépréciées
-   * - double-invoque certains hooks pour détecter les bugs
-   * En StrictMode, useEffect() s'exécute 2 fois en développement
-   **/
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
+  <ChakraProvider theme={theme}>
       <App />
-    </ChakraProvider>
-  </React.StrictMode>,
+  </ChakraProvider>
 );
